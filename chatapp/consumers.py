f5a5c714273f2ecc0cf10961fe21ebdac65ae974
arhamfareed106 <arhamfareed575@gmail.com>
2024-10-30 17:00:09 +0500

    create room

---
+++ chatapp/consumers.py
@@ -0,0 +1,13 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from chatapp.models import *
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+     async def connect(self):
+          self.room_name= f"room_{self.scope['url_route']['kwargs']['room_name']}"
+          await self.channel_layer.group_add(self.room_name, self.channel_name)
+          await self.accept()
+     async def disconnect(self, clode_code):
+          await self.channels_layer.group_discard(self.room_name, self.channel_name)
--- chatapp/views.py
+++ chatapp/views.py
@@ -1,28 +1,27 @@
 from django.shortcuts import render, redirect
-from .models import Room  # type: ignore
+from .models import Room, Message # type: ignore
 
 def CreateRoom(request):  
     if request.method == 'POST':
         username = request.POST.get('username') 
         room = request.POST.get('room')
         
-        # Check if the room exists; otherwise, create a new room
         try:
             get_room = Room.objects.get(room_name=room)
             return redirect('room', room_name=get_room.room_name, username=username)
         except Room.DoesNotExist:
-            # Room doesn't exist, so create and save it
             new_room = Room(room_name=room)
             new_room.save()
-            return redirect('room', room_name=new_room.room_name, username=username)
+            return redirect('room', room_name=room, username=username)
 
-    # Render the default form for GET request
     return render(request, 'index.html')
 
 def MessageView(request, room_name, username):
-    # Pass room name and username to the chat view
+    get_room= Room.objects.get(room_name=room_name)
+    get_message = Message.objects.filter(room=get_room)
     context = {
         'room_name': room_name,
-        'username': username,
+        'user': username,
+        'messages': get_message
     }
     return render(request, '_message.html', context)
